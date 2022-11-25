f9a24ef14e528983b4cccf46ec92597def2ecc2b
Milena Sateles <milenamcsateles@gmail.com>
2022-11-25 13:33:18 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace DivisaoImpossivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lê um par de números e divide o primeiro pelo segundo
+
+            Console.WriteLine("Quantos pares de números?");
+            int quant = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < quant; i++)
+            {
+                Console.Write("Primeiro número: ");
+                double num = double.Parse(Console.ReadLine()!);
+                Console.Write("Segundo número: ");
+                double deno = double.Parse(Console.ReadLine()!);
+
+                if (deno == 0)
+                    Console.WriteLine("Divisão impossível");
+
+                else
+                    Console.WriteLine("Resultado: " + (num / deno).ToString("F1"), CultureInfo.InvariantCulture);
+               
+            }
+
+        }
+    }
+}
